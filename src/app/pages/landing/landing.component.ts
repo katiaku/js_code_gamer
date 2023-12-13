0e4baf92de3fce0c687ca759043daa16db83a5c7
Marco-Wiens <marco.wiens@gmail.com>
2023-12-13 11:13:57 +0100

    cambios landing

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit{
+
+  constructor(public componentApp:AppComponent){}
+
+
+  ngOnInit(): void {
+    
+    this.componentApp.mostrarHeader = true;
+  }
+
+
 
 }
