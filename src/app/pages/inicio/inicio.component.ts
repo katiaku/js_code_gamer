8e2212c9c7dbebc4a4ef48ffefb144fda13f6b0f
Marco-Wiens <marco.wiens@gmail.com>
2023-12-13 14:28:53 +0100

    pag register terminada sin funcionalidad

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit{
 
+
+  constructor(public router:Router, public componenteApp:AppComponent){}
+
+
+  ngOnInit(): void {
+    this.componenteApp.mostrarHeader = false;
+  }
+
+  start(){
+    this.router.navigate(['/landing']);
+  }
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
 
+  constructor(public componentApp:AppComponent){
+    
+  }
+  ngOnInit(): void {
+    this.componentApp.mostrarHeader = true;
+  }
+
+
+  onSubmit(){
+    
+  }
 }
