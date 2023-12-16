5e2a9a83be308d016ab2f95fba4d4135c598abd9
Katia <kushnir.ekaterina@gmail.com>
2023-12-16 19:14:31 +0100

    Correccion animaciones

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, Renderer2 } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppComponent } from 'src/app/app.component';
 
@@ -7,7 +7,9 @@ import { AppComponent } from 'src/app/app.component';
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent implements OnInit, AfterViewInit {
+export class InicioComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  private starryBackground: HTMLDivElement | null = null;
 
   constructor(public router: Router, public componenteApp: AppComponent, private renderer: Renderer2) { }
 
@@ -19,12 +21,14 @@ export class InicioComponent implements OnInit, AfterViewInit {
     this.createStars();
   }
 
+  ngOnDestroy(): void {
+    this.removeStars();
+  }
+
   private createStars(): void {
-    const starryBackground = this.renderer.createElement('div');
-    this.renderer.addClass(starryBackground, 'starry-background');
-
+    this.starryBackground = this.renderer.createElement('div');
+    this.renderer.addClass(this.starryBackground, 'starry-background');
     const numberOfStars = 60;
-
     for (let i = 0; i < numberOfStars; i++) {
       const star = this.renderer.createElement('div');
       const randomDelay = Math.random() * 10;
@@ -32,10 +36,16 @@ export class InicioComponent implements OnInit, AfterViewInit {
       this.renderer.setStyle(star, 'top', `${Math.random() * 40 + 10}%`);
       this.renderer.setStyle(star, 'left', `${Math.random() * 100}%`);
       this.renderer.setStyle(star, 'animation-delay', `${randomDelay}s`);
-      this.renderer.appendChild(starryBackground, star);
+      this.renderer.appendChild(this.starryBackground, star);
     }
+    this.renderer.appendChild(document.body, this.starryBackground);
+  }
 
-    this.renderer.appendChild(document.body, starryBackground);
+  private removeStars(): void {
+    if (this.starryBackground) {
+      this.renderer.removeChild(document.body, this.starryBackground);
+      this.starryBackground = null;
+    }
   }
 
   start() {
