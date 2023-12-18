ac8710273c4bbd114aed16ead5014f4fdf94a30f
Katia <kushnir.ekaterina@gmail.com>
2023-12-18 09:50:37 +0100

    Modelos User y Level

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,2 +1,17 @@
+import { Level } from "./level";
+
 export class User {
+
+  nickname: string;
+  email: string;
+  password: string;
+  levels: Level[];
+
+  constructor(nickname: string, email: string, password: string, levels: Level[]) {
+    this.nickname = nickname;
+    this.email = email;
+    this.password = password;
+    this.levels = levels;
+  }
+
 }
